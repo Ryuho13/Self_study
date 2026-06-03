e5f208c75d6508a4d93d1ac152c47793fc3af1b3
Ryuho13 <hosedk99@gmail.com>
2026-06-03 21:37:06 +0900

    260603

--- algorithm/Algorithm.java
+++ algorithm/Algorithm.java
@@ -512,3 +512,71 @@ class Solution {
         return new String(chars);
     }
 }
+
+// 문제 7: 
+
+// 문제 설명
+// 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
+
+// 제한사항
+// prices의 각 가격은 1 이상 10,000 이하인 자연수입니다.
+// prices의 길이는 2 이상 100,000 이하입니다.
+// 입출력 예
+// prices	return
+// [1, 2, 3, 2, 3]	[4, 3, 1, 1, 0]
+// 입출력 예 설명
+// 1초 시점의 ₩1은 끝까지 가격이 떨어지지 않았습니다.
+// 2초 시점의 ₩2은 끝까지 가격이 떨어지지 않았습니다.
+// 3초 시점의 ₩3은 1초뒤에 가격이 떨어집니다. 따라서 1초간 가격이 떨어지지 않은 것으로 봅니다.
+// 4초 시점의 ₩2은 1초간 가격이 떨어지지 않았습니다.
+// 5초 시점의 ₩3은 0초간 가격이 떨어지지 않았습니다.
+// ※ 공지 - 2019년 2월 28일 지문이 리뉴얼되었습니다.
+
+// 답 :
+// 스택에 인덱스를 쌓으면서, 현재 가격이 스택 top의 가격보다 낮으면(= 가격 하락)
+// pop하여 "하락 직전까지의 초 수"를 기록. 끝까지 남은 인덱스는 마지막까지 버텼으므로 (n-1-i).
+
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - 1 - idx;
+        }
+
+        return answer;
+    }
+}
+
+// 다른 사람 풀이  :
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] answer = new int[len];
+        int i, j;
+        for (i = 0; i < len; i++) {
+            for (j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j])
+                    break;
+            }
+        }
+        return answer;
+    }
+}
+
+
